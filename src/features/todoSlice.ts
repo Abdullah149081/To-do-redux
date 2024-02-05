1ba55415a93970b9eadde0750bddf08c548d74d4
Abdullah149081 <masudmd425@gmail.com>
2024-02-05 23:20:25 +0600

    Refactor todoSlice.ts to use TodoItem interface and add new actions

--- src/features/todoSlice.ts
+++ src/features/todoSlice.ts
@@ -1,7 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "@/Redux/store";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface TodoItem {
+  id: string;
+  title: string;
+  description: string;
+  isCompleted?: boolean;
+}
 
 interface TodoState {
-  todoList: string[];
+  todoList: TodoItem[];
 }
 
 const initialState: TodoState = {
@@ -11,7 +19,20 @@ const initialState: TodoState = {
 export const todoSlice = createSlice({
   name: "todos",
   initialState,
-  reducers: {},
+  reducers: {
+    addTodo: (state, action: PayloadAction<TodoItem>) => {
+      state.todoList.push({ ...action.payload, isCompleted: false });
+    },
+    removeTodo: (state, action: PayloadAction<string>) => {
+      state.todoList = state.todoList.filter(
+        (todo) => todo.id !== action.payload
+      );
+    },
+  },
 });
+
+export const selectCount = (state: RootState) => state.todos.todoList;
+
+export const { addTodo, removeTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
