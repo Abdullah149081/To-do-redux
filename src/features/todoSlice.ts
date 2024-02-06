5befdba48a0c5c7430229b40d94ec791c3435c5f
Abdullah149081 <masudmd425@gmail.com>
2024-02-06 23:25:01 +0600

    Add sorting and toggle functionality to todo list

--- src/features/todoSlice.ts
+++ src/features/todoSlice.ts
@@ -22,17 +22,35 @@ export const todoSlice = createSlice({
   reducers: {
     addTodo: (state, action: PayloadAction<TodoItem>) => {
       state.todoList.push({ ...action.payload, isCompleted: false });
+      state.todoList.sort((a, b) => {
+        return a.isCompleted === b.isCompleted ? 0 : a.isCompleted ? 1 : -1;
+      });
     },
     removeTodo: (state, action: PayloadAction<string>) => {
       state.todoList = state.todoList.filter(
         (todo) => todo.id !== action.payload
       );
     },
+    toggleCompleted: (state, action: PayloadAction<string>) => {
+      const todo = state.todoList.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.isCompleted = !todo.isCompleted;
+        state.todoList.sort((a, b) => {
+          if (a.isCompleted && !b.isCompleted) {
+            return 1;
+          } else if (!a.isCompleted && b.isCompleted) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+      }
+    },
   },
 });
 
 export const selectCount = (state: RootState) => state.todos.todoList;
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, toggleCompleted } = todoSlice.actions;
 
 export default todoSlice.reducer;
