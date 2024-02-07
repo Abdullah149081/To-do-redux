656eb00ecfe10841a0a340dddb428c3906e93eff
Abdullah149081 <masudmd425@gmail.com>
2024-02-07 11:51:42 +0600

    Add todoApi to store configuration

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,10 +1,15 @@
+import { todoApi } from "@/api/todo";
 import todoReducer from "@/features/todoSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
+    [todoApi.reducerPath]: todoApi.reducer,
     todos: todoReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todoApi.middleware),
+    
 });
 
 export type RootState = ReturnType<typeof store.getState>;
