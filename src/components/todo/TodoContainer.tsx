820d770242a5f3dc815ab8f3c7aeb82e0e2a0aed
Abdullah149081 <masudmd425@gmail.com>
2024-02-07 20:29:44 +0600

    Refactor TodoContainer to use useGetTodosQuery hook

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,12 +1,14 @@
 import { useAppSelector } from "@/Redux/hooks";
 
+import { useGetTodosQuery } from "@/api/todo";
 import { selectCount } from "@/features/todoSlice";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
-  const todos = useAppSelector(selectCount);
+  // const todos = useAppSelector(selectCount);
+  const { data: todos } = useGetTodosQuery();
 
   return (
     <div>
@@ -16,8 +18,8 @@ const TodoContainer = () => {
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-          {todos?.map((item) => (
-            <TodoCard key={item.id} {...item} />
+          {todos?.data.map((item) => (
+            <TodoCard key={item._id} {...item} />
           ))}
         </div>
         {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
