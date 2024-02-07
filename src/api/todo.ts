6774f8636ed479f0d88f451f749beefb40a3ac88
Abdullah149081 <masudmd425@gmail.com>
2024-02-07 20:29:01 +0600

    Add CRUD operations and mutation hooks for todoApi

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -3,11 +3,41 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const todoApi = createApi({
   reducerPath: "todoApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  tagTypes: ["todo"],
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => ({ url: "tasks", method: "GET" }),
+      providesTags: ["todo"],
+    }),
+    postTodo: builder.mutation({
+      query: (data) => ({
+        url: "task",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["todo"],
+    }),
+    removeTodo: builder.mutation({
+      query: (id) => ({
+        url: `task/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["todo"],
+    }),
+    updateTodo: builder.mutation({
+      query: (data) => ({
+        url: `task/${data._id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["todo"],
     }),
   }),
 });
 
-export const { useGetTodosQuery } = todoApi;
+export const {
+  useGetTodosQuery,
+  usePostTodoMutation,
+  useRemoveTodoMutation,
+  useUpdateTodoMutation,
+} = todoApi;
