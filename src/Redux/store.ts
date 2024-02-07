e3c67c7ed3470f8f1a049875bb9a0a3ab3f364c2
Abdullah149081 <masudmd425@gmail.com>
2024-02-07 12:03:43 +0600

    Refactor Redux store middleware

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -2,14 +2,15 @@ import { todoApi } from "@/api/todo";
 import todoReducer from "@/features/todoSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
+const middleware = [todoApi.middleware];
+
 export const store = configureStore({
   reducer: {
     [todoApi.reducerPath]: todoApi.reducer,
     todos: todoReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(todoApi.middleware),
-    
+    getDefaultMiddleware().concat(middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
